90a1b7c54eb00d1a645402b806c489c6c347a914
Nstjacques <Nstjacques15bf@gmail.com>
2019-03-18 14:13:41 -0700

    Basic Doorway Teleport Set-Up
    Exactly what it says

---
+++ UncannyTesting/Assets/_Scripts/doorwayTeleportation.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorwayTeleportation : MonoBehaviour {
+
+    public bool inDoorTrigger = false;
+
+    public GameObject player;
+
+    public Vector3 warpPoint;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (inDoorTrigger == true && Input.GetKeyDown(KeyCode.E))
+        {
+            Time.timeScale = 0;
+            StartCoroutine(teleportWait());
+        }
+	}
+
+    IEnumerator teleportWait()
+    {
+        yield return new WaitForSecondsRealtime(2);
+        player.transform.position = warpPoint;
+        Time.timeScale = 1;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("inTrigger is true");
+            inDoorTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("inTrigger is false");
+            inDoorTrigger = false;
+        }
+    }
+}
