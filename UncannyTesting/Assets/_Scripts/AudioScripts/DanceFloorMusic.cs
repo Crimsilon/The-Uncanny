c334daa08695a27dce303e821f3eca6dbd69bf64
Nstjacques <Nstjacques15bf@gmail.com>
2019-05-03 15:00:32 -0700

    More Audio Work
    Added volumes and scripts to call different music for different rooms.

---
+++ UncannyTesting/Assets/_Scripts/AudioScripts/DanceFloorMusic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DanceFloorMusic : MonoBehaviour {
+
+    public Osborne_AudioManager_CyberpunkNoir music;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Not muffled music");
+            music.NotMuffled();
+            music.AmbienceRoom();
+            music.DefaultAmbience();
+        }
+    }
+}
---
+++ UncannyTesting/Assets/_Scripts/AudioScripts/SlightlyMuffledBathroomClubScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlightlyMuffledBathroomClubScript : MonoBehaviour
+{
+
+    public Osborne_AudioManager_CyberpunkNoir music;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Muffled bathroom music");
+            music.BitMuffled();
+            music.AmbienceRoom();
+            music.BathroomAmbience();
+        }
+    }
+}
