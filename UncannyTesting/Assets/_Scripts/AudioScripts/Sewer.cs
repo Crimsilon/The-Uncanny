2c301d0e83c2e77dd3b352499012bf2fdfb2fbad
Nstjacques <Nstjacques15bf@gmail.com>
2019-05-03 16:28:07 -0700

    More Audio Scripts
    Made scripts for the Sneaking Room and Sewers

---
+++ UncannyTesting/Assets/_Scripts/AudioScripts/Sewer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sewer : MonoBehaviour {
+
+    public Osborne_AudioManager_CyberpunkNoir music;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            music.SewerAmbienceAndMusic();
+        }
+    }
+}
