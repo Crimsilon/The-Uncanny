82b09cca952a4a6e8dff74f42bc39cd894a1e325
Nstjacques <Nstjacques15bf@gmail.com>
2019-05-11 11:13:54 -0700

    Finished Intro Cutscene

--- UncannyTesting/Assets/_Scripts/introScene.cs
+++ UncannyTesting/Assets/_Scripts/introScene.cs
@@ -13,7 +13,9 @@ public class introScene : MonoBehaviour {
 
     public GameObject dialogueBox;
 
-    public Dialogue introScript;
+    public Dialogue introScript1;
+
+    public Dialogue introScript2;
 
     public playerController playerController;
 
@@ -29,24 +31,53 @@ public class introScene : MonoBehaviour {
 
     Vector3 playerCurrent;
 
-    public bool finishWalking = false;
+    public bool finishWalking1 = false;
+
+    public bool finishWalking2 = false;
+
+    public int dialogueCount1 = 0;
+
+    public int totalDialogue1 = 2;
+
+    public int dialogueCount2 = 0;
+
+    public int totalDialogue2 = 2;
+
+    public bool canInteract;
+
+    public bool firstHalf = true;
 
     // Use this for initialization
     void Start () {
         m_ZAxis = new Vector3(0, 0, 3.125f);
         playerStart = player.transform.position;
         StartCoroutine(introCutscene());
+        canInteract = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
         playerCurrent = player.transform.position;
 
+        if (playerCurrent.z >= playerStart.z + 1)
+        {
+            finishWalking1 = true;
+        }
+
         if (playerCurrent.z >= playerStart.z + 2)
         {
-            finishWalking = true;
+            finishWalking2 = true;
         }
-	}
+
+        if (Input.GetKeyDown(KeyCode.Space) && canInteract == true && firstHalf == true)
+        {
+            dialogueCount1 = dialogueCount1 + 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && canInteract == true && firstHalf == false)
+        {
+            dialogueCount2 = dialogueCount2 + 1;
+        }
+    }
 
     IEnumerator introCutscene()
     {
@@ -55,13 +86,34 @@ public class introScene : MonoBehaviour {
         playerBody.velocity = m_ZAxis;
         Quaternion rotation = Quaternion.LookRotation(Vector3.forward, Vector3.up);
         player.transform.rotation = rotation;
-        yield return new WaitUntil(() => finishWalking == true);
+        playerBody.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
+        yield return new WaitUntil(() => finishWalking1 == true);
         playerAnim.SetBool("isWalking", false);
+        playerBody.constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotation;
         yield return new WaitForSecondsRealtime(1f);
+        canInteract = true;
         dialogueBox.SetActive(true);
-        FindObjectOfType<dialogueManager>().StartDialogue(introScript);
+        FindObjectOfType<dialogueManager>().StartDialogue(introScript1);
         Time.timeScale = 0;
-        yield return new WaitForSecondsRealtime(0.1f);
+        yield return new WaitUntil(() => dialogueCount1 >= totalDialogue1);
+        firstHalf = false;
+        canInteract = false;
+        dialogueBox.SetActive(false);
+        Time.timeScale = 1;
+        playerBody.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
+        playerAnim.SetBool("isWalking", true);
+        playerBody.velocity = m_ZAxis;
+        player.transform.rotation = rotation;
+        yield return new WaitUntil(() => finishWalking2 == true);
+        playerAnim.SetBool("isWalking", false);
+        playerBody.constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotation;
+        yield return new WaitForSecondsRealtime(0.5f);
+        canInteract = true;
+        dialogueBox.SetActive(true);
+        FindObjectOfType<dialogueManager>().StartDialogue(introScript2);
+        Time.timeScale = 0;
+        yield return new WaitUntil(() => dialogueCount2 >= totalDialogue2);
+        canInteract = false;
         dialogueBox.SetActive(false);
         Time.timeScale = 1;
         playerController.inScene = false;
